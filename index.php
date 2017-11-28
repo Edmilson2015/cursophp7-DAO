10ee1b9d18157fef0290f5a0dec42842843e7716
Edmilson Martins <edmilson2015@hotmail.com>
2017-11-28 16:10:56 -0300

    Atualizando

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 
 require_once("config.php");
 
-$sql = new Sql();
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-echo json_encode($usuarios);
+$root = new usuario();
+$root->loadById(3);
+echo $root;
 
 
  ?>
