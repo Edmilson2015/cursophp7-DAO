57b726430c69e821de6dead097155cc5f25e77c2
Edmilson Martins <edmilson2015@hotmail.com>
2017-11-28 16:54:50 -0300

    Atualizando

--- index.php
+++ index.php
@@ -3,9 +3,27 @@
 
 require_once("config.php");
 
-$root = new usuario();
-$root->loadById(3);
-echo $root;
+// carrega 1 usuario
+//$root = new usuario();
+//$root->loadById(3);
+//echo $root;
+
+//Carrega lista de usuarios
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
 
 
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+
+//carrega um usuario autenticado com login e senha
+
+$usuario = new Usuario();
+$usuario->login("root", "123");
+
+echo $usuario;
  ?>
